9b4b8d59c34c11f8c94ff3258cd09b73e97ba897
KolyaPronin <kolya.pronin.0506@mail.ru>
2026-04-15 20:28:54 +0700

    added swagger config

--- src/main/java/com/kolya/TaskManager/config/SwaggerConfig.java
+++ src/main/java/com/kolya/TaskManager/config/SwaggerConfig.java
@@ -1,4 +1,24 @@
 package com.kolya.TaskManager.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI swaggerApi(){
+        return new OpenAPI()
+                .servers(
+                        List.of(
+                                new Server().url("http://localhost:8080")
+                        )
+                )
+                .info(
+                        new Info().title("Task manager api")
+                );
+    }
 }
--- src/main/java/com/kolya/TaskManager/controller/MainController.java
+++ src/main/java/com/kolya/TaskManager/controller/MainController.java
@@ -2,11 +2,14 @@ package com.kolya.TaskManager.controller;
 
 import com.kolya.TaskManager.dto.TaskDto;
 import com.kolya.TaskManager.service.impl.TaskServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "main_endpoints" )
 @Slf4j
 @RestController
 @RequestMapping("/api/task")
@@ -16,26 +19,32 @@ public class MainController {
         this.service = service;
     }
 
+    @Operation(summary = "Fetching task by id", description = "The request contains the ID" +
+            " of the task being received.")
     @GetMapping("/get/{id}")
     public TaskDto getById(@PathVariable Long id){
         return service.getTaskById(id);
     }
 
+    @Operation(summary = "Fetching all existing tasks")
     @GetMapping("/getAll")
     public List<TaskDto> getAll() {
         return service.getAll();
     }
 
+    @Operation(summary = "Create new task", description = "Receives the task DTO and saves it to the database")
     @PostMapping("/create")
     public TaskDto create(@RequestBody TaskDto dto) {
         return service.create(dto);
     }
 
+    @Operation(summary = "delete task by id", description = "Receives the task id and deleting the task from db")
     @DeleteMapping("/delete/{id}")
     public String delete(@PathVariable Long id) {
         return service.delete(id);
     }
 
+    @Operation(summary = "update task", description = "Receives the task id and dto and updating task")
     @PutMapping("/update/{id}")
     public TaskDto update(@PathVariable Long id, @RequestBody TaskDto dto) {
         return service.update(id, dto);
