021c123cf191f3b81d46e27ee79b6e9f2af05f25
KolyaPronin <kolya.pronin.0506@mail.ru>
2026-04-17 18:55:01 +0700

    404 exception handler added

--- src/main/java/com/kolya/TaskManager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/kolya/TaskManager/exception/GlobalExceptionHandler.java
@@ -10,6 +10,22 @@ import java.time.LocalDateTime;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFound(TaskNotFoundException ex, HttpServletRequest request) {
+
+        log.warn("Not found: {}", ex.getMessage());
+
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.NOT_FOUND.value(),
+                "Not Found",
+                ex.getMessage(),
+                request.getRequestURI()
+        );
+    }
+
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(Exception ex, HttpServletRequest request) {
